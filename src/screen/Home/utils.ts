790f3ab091355fab97897761bfafd47c5ed3fd26
dev <dev.reactjs@gmail.com>
2020-08-21 14:18:27 +0530

    fixed single filter issue

--- src/screen/Home/utils.ts
+++ src/screen/Home/utils.ts
@@ -3,27 +3,36 @@ import { filterType, ITEM } from "../../types";
 export function filterData(filters: filterType, data: Array<ITEM>) {
     const { additionalOptions, location, type } = filters;
     const options = Object.values(additionalOptions).map(data => data.checked && data.label);
-    if(options.length && location && type) {
+    const validOption = options.filter(data => data);
+    if(validOption.length && location && type) {
         return data.filter((item) => (
             item.location.toLowerCase().includes(location.toLowerCase())
             && (item.type === type)
-            && (options.includes(item.category))
+            && (validOption.includes(item.category))
         ));
-    } else if(options.length && location) {
+    } else if(validOption.length && location) {
         return data.filter(item => (
             item.location.toLowerCase().includes(location.toLowerCase())
-            && (options.includes(item.category))
+            && (validOption.includes(item.category))
         ));
-    } else if(options.length && type) {
+    } else if(validOption.length && type) {
         return data.filter(item => (
             (item.type === type)
-            && (options.includes(item.category))
+            && (validOption.includes(item.category))
         ));
-    }  else if(location && type) {
+    } else if(location && type) {
         return data.filter(item => (
             item.location.toLowerCase().includes(location.toLowerCase())
             && (item.type === type)
         ));
+    } else if(validOption.length) {
+        return data.filter(item => ((validOption.includes(item.category))));
+    } else if(location) {
+        return data.filter(item => (
+            item.location.toLowerCase().includes(location.toLowerCase())
+        ));
+    } else if(type) {
+        return data.filter(item => ((item.type === type)));
     } else {
         return data;
     }
